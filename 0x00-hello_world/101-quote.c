c0b073296d5fd591ded5259448aed31b7a61ea80
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2022-06-05 15:37:33 -0700

    yay

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
-*/
+ * Return: Always 1 (Error)
+ */
 
 int main(void)
 {
-	fprintf(stderr,"and that piece of art is useful - Dora Korpar, 2015-10-19\n"); 
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
