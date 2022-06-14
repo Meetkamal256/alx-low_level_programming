714bf898490d3c820cfaa51e7f2b844f5c2da95f
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2022-06-14 12:02:17 -0700

    for me

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,30 +1,14 @@
 #include "main.h"
 /**
-<<<<<<< HEAD
- * swap_int - updates the value it points to 98.
- * @a: input integer
+ * swap_int - updates the value it points to to 98.
+ * @a: input integer.
  * @b: input integer
  * Return: no return.
  */
 void swap_int(int *a, int *b)
 {
     int buffer;
-    
     buffer = *a;
     *a = *b;
     *b = buffer;
-=======
- * swap_int - swapping the values of two integers
- *@a: 1st integer is swapped
- *@b: 2nd integer is swapped
- * Return: Always 0.
- */
-void swap_int(int *a, int *b)
-{
-	int buffer;
-
-	buffer = *a;
-	*a = *b;
-	*b = buffer;
->>>>>>> bbd551c628649292391dd371a204d5bbaa736869
 }
