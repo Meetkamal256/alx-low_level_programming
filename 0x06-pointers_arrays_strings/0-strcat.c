5b6acdf50a36a494524498356bec9f11ee5fffd5
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2022-06-16 23:30:52 +0100

    a

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,15 +7,15 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    int i, j;
-    for (i = 0; dest[i] != '\0'; i++)
-    {
-	    continue;
-    }
-    for (j = 0; src[j] != '\0'; i++, j++)
-    {
-	    dest[i] = src[j];
-    }
-    dest[i] = '\0';
-    return (dest);
+	int i, j;
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		continue;
+	}
+	for (j = 0; src[j] != '\0'; i++, j++)
+	{
+		dest[i] = src[j];
+	}
+	dest[i] = '\0';
+	return (dest);
 }
